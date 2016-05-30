5e26146cb8e68fe1c0adec2a9ae4dadb5b2091b3
Paris <Parysus@users.noreply.github.com>
2016-05-30 09:41:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var port = Number( process.env.PORT || 3000)
 
-app.use(expr)
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.send('<h1>Hello world</h1>');
